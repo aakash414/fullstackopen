6ada98bec242354bdd884488af9837caaa16238f
aakash414 <67820270+aakash414@users.noreply.github.com>
2023-07-08 00:03:40 +0530

    2.11 axios

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,15 +1,11 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
 
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
@@ -28,6 +24,10 @@ const App = () => {
   const handleNameFilterChange = (e) => setNameFilter(e.target.value);
   const handleNameChange = (e) => setNewName(e.target.value);
   const handleNewNumberChange = (event) => setNewNumber(event.target.value);
+
+  useEffect(() => {
+    axios.get("//localhost:3001/persons").then((res) => setPersons(res.data));
+  }, []);
 
   return (
     <div>
