a1a54a941d725225e8d029f387e5dfa71c4a2470
aakash414 <67820270+aakash414@users.noreply.github.com>
2024-02-09 15:07:29 +0530

    2.16

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,16 +1,17 @@
-// App.js
-
 import { useState, useEffect } from "react";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
 import personService from "./services/personService";
+import Notification from "./components/Notification";
+import "./index.css";
 
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [nameFilter, setNameFilter] = useState("");
+  const [notification, setNotification] = useState(null);
 
   useEffect(() => {
     personService.getAll().then((initialPersons) => {
@@ -38,17 +39,27 @@ const App = () => {
                 person.id !== existingPerson.id ? person : returnedPerson
               )
             );
+            showNotification(`Number updated for ${newName}`);
             setNewName("");
             setNewNumber("");
+          })
+          .catch((error) => {
+            showErrorNotification(`Failed to update number for ${newName}`);
           });
       }
     } else {
       const newPerson = { name: newName, number: newNumber };
-      personService.create(newPerson).then((returnedPerson) => {
-        setPersons(persons.concat(returnedPerson));
-        setNewName("");
-        setNewNumber("");
-      });
+      personService
+        .create(newPerson)
+        .then((returnedPerson) => {
+          setPersons(persons.concat(returnedPerson));
+          showNotification(`Added ${newName}`);
+          setNewName("");
+          setNewNumber("");
+        })
+        .catch((error) => {
+          showErrorNotification(`Failed to add ${newName}`);
+        });
     }
   };
 
@@ -56,9 +67,23 @@ const App = () => {
   const handleNameChange = (e) => setNewName(e.target.value);
   const handleNewNumberChange = (event) => setNewNumber(event.target.value);
 
+  const showNotification = (message) => {
+    setNotification(message);
+    setTimeout(() => {
+      setNotification(null);
+    }, 5000);
+  };
+
+  const showErrorNotification = (errorMessage) => {
+    showNotification(errorMessage);
+    console.error(errorMessage);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={notification} />{" "}
+      {/* Use the Notification component here */}
       <Filter value={nameFilter} onChange={handleNameFilterChange} />
       <h2>Add New</h2>
       <PersonForm
