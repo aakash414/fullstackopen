4fa594b9ff1b94daf555a816fa8e1b4f772551c4
aakash414 <67820270+aakash414@users.noreply.github.com>
2024-02-14 23:27:55 +0530

    3.9-3.11

--- part3/index.js
+++ part3/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const cors = require("cors");
+
+app.use(cors());
 app.use(express.json());
 app.use(morgan("tiny"));
 
@@ -89,6 +92,6 @@ app.delete("/api/persons/:id", (request, response) => {
   response.status(204).end();
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT);
 console.log(`Server running on ${PORT}`);
---
+++ part3/readme.md
@@ -0,0 +1,3 @@
+# 3.11
+
+https://65ccfec10ad3bc0a1bd30a61--jocular-griffin-ee3026.netlify.app/
