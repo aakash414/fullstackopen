849ed51fa091c441a19a2484dececf4a00e78a11
aakash414 <67820270+aakash414@users.noreply.github.com>
2024-02-25 09:35:03 +0530

    3.19-3.21

--- part3/phonebook_backend/models/person.js
+++ part3/phonebook_backend/models/person.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
-
-mongoose.set("strictQuery", false);
+const uniqueValidator = require("mongoose-unique-validator");
 
 const url = process.env.MONGODB_URI;
-console.log("connecting to", url);
 
 mongoose
-  .connect(url)
+  .connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
   .then((result) => {
     console.log("connected to MongoDB");
   })
@@ -15,8 +18,18 @@ mongoose
   });
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: Number,
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: [3, "Name must have atleast 3 characters"],
+  },
+  number: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: [8, "Name must have atleast 8 characters"],
+  },
 });
 
 personSchema.set("toJSON", {
@@ -26,5 +39,7 @@ personSchema.set("toJSON", {
     delete returnedObject.__v;
   },
 });
+
+personSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model("Person", personSchema);
