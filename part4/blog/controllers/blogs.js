84f7d8c30940e3f061f7b27df6181e5948f15345
aakash414 <67820270+aakash414@users.noreply.github.com>
2024-03-02 11:07:23 +0530

    4.13-4.14

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -26,4 +26,25 @@ blogsRouter.post("/", (request, response, next) => {
     .catch((error) => next(error));
 });
 
+blogsRouter.put("/:id", async (request, response) => {
+  const body = request.body;
+
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+  };
+
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
+    new: true,
+  });
+  response.json(updatedBlog);
+});
+
+blogsRouter.delete("/:id", async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id);
+  response.status(204).end();
+});
+
 module.exports = blogsRouter;
