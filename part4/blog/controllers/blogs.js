312a924e91fa7ac14a6a55520d739897d6344b9c
aakash414 <67820270+aakash414@users.noreply.github.com>
2024-03-02 11:03:29 +0530

    4.9-4.12

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -6,6 +6,15 @@ blogsRouter.get("/", async (request, response) => {
   response.json(blogs.map((blog) => blog.toJSON()));
 });
 
+blogsRouter.get("/:id", async (request, response) => {
+  const blog = await Blog.findById(request.params.id);
+  if (blog) {
+    response.json(blog);
+  } else {
+    response.status(404).end();
+  }
+});
+
 blogsRouter.post("/", (request, response, next) => {
   const blog = new Blog(request.body);
 
