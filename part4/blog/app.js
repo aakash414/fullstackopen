2d02503d810b53b22e1b8f98d4935bdc49cfba65
aakash <aakashpr975@gmail.com>
2024-03-02 20:13:33 +0530

    4.15-4.16

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -1,36 +1,55 @@
+/* eslint-disable linebreak-style */
 const config = require("./utils/config");
+
 const express = require("express");
+
 const app = express();
+
 require("express-async-errors");
+const usersRouter = require("./controllers/users");
 
 const cors = require("cors");
+
 const blogsRouter = require("./controllers/blogs");
+
 const middleware = require("./utils/middleware");
+
 const logger = require("./utils/logger");
+
 const mongoose = require("mongoose");
 
 mongoose
+
   .connect(config.MONGODB_URI, {
     useNewUrlParser: true,
+
     useUnifiedTopology: true,
+
     useFindAndModify: false,
+
     useCreateIndex: true,
   })
+
   .then(() => {
     logger.info("connected to MongoDB");
   })
+
   .catch((error) => {
     logger.error("error connecting to MongoDB:", error.message);
   });
 
 app.use(cors());
-app.use(express.static("build"));
-app.use(express.json());
-app.use(middleware.requestLogger);
 
+app.use(express.static("build"));
+
+app.use(express.json());
+
+app.use(middleware.requestLogger);
+app.use("/api/users", usersRouter);
 app.use("/api/blogs", blogsRouter);
 
 app.use(middleware.unknownEndpoint);
+
 app.use(middleware.errorHandler);
 
 module.exports = app;
--- part4/blog/tests/api/test_helper.js
+++ part4/blog/tests/api/test_helper.js
@@ -1,41 +1,159 @@
+/* eslint-disable linebreak-style */
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+const bcrypt = require("bcrypt");
+const app = require("../../app");
+const api = supertest(app);
+const helper = require("./test_helper");
 const Blog = require("../../models/blog");
+const User = require("../../models/user");
 
-const initialBlogs = [
-  {
-    title: "React patterns",
-    author: "Michael Chan",
-    url: "https://reactpatterns.com/",
-    likes: 7,
-  },
-  {
-    title: "Go To Statement Considered Harmful",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-    likes: 5,
-  },
-];
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  await Blog.insertMany(helper.initialBlogs);
+});
 
-const nonExistingId = async () => {
-  const blog = new Blog({
-    title: "willremovethissoon",
-    author: "Jhon Doe",
-    url: "https://fullstackopen.com/",
-    likes: 4,
+describe("when there is initially some blogs saved", () => {
+  test("blogs are returned as json", async () => {
+    await api
+      .get("/api/blogs")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  });
+});
+
+describe("viewing a specific note", () => {
+  test("query if a blog has id or _id", async () => {
+    const singleBlog = await helper.blogsInDb();
+
+    expect(singleBlog[0].id).toBeDefined();
+    expect(singleBlog[0]._id).toBe(undefined);
+  });
+});
+
+describe("addition of a new blog", () => {
+  test("a valid blog can be added", async () => {
+    const newBlog = {
+      title: "Test an app",
+      author: "Jhon Doe",
+      url: "https://fullstackopen.com/",
+      likes: 4,
+    };
+
+    await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+
+    const blogsAtEnd = await helper.blogsInDb();
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+
+    const titles = blogsAtEnd.map((t) => t.title);
+    expect(titles).toContain("Test an app");
   });
 
-  await blog.save();
-  await blog.remove();
+  test("verify if likes property is missing then it defaults to 0", async () => {
+    const newBlog = {
+      title: "Test an app",
+      author: "Jhon Doe",
+      url: "https://fullstackopen.com/",
+    };
 
-  return blog._id.toString();
-};
+    const response = await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
 
-const blogsInDb = async () => {
-  const blogs = await Blog.find({});
-  return blogs.map((blog) => blog.toJSON());
-};
+    expect(response.body.likes).toBeDefined();
+    expect(response.body.likes).toBe(0);
+  });
 
-module.exports = {
-  initialBlogs,
-  nonExistingId,
-  blogsInDb,
-};
+  test("blog without url is not added", async () => {
+    const newBlog = {
+      title: "Test an app",
+      author: "Jhon Doe",
+    };
+
+    await api.post("/api/blogs").send(newBlog).expect(400);
+
+    const blogsAtEnd = await helper.blogsInDb();
+
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length);
+  });
+});
+
+describe("deletion of a blog", () => {
+  test("succeeds with status code 204 if id is valid", async () => {
+    const blogsAtStart = await helper.blogsInDb();
+    const blogToDelete = blogsAtStart[0];
+
+    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
+
+    const blogsAtEnd = await helper.blogsInDb();
+
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1);
+
+    const titles = blogsAtEnd.map((r) => r.title);
+
+    expect(titles).not.toContain(blogToDelete.title);
+  });
+});
+
+describe("when there is initially one user at db", () => {
+  beforeEach(async () => {
+    await User.deleteMany({});
+
+    const passwordHash = await bcrypt.hash("password", 10);
+    const user = new User({ username: "root", passwordHash });
+
+    await user.save();
+  });
+
+  test("creation succeeds with a fresh username", async () => {
+    const usersAtStart = await helper.usersInDb();
+
+    const newUser = {
+      username: "kayra",
+      name: "Kayra Berk Tuncer",
+      password: "password",
+    };
+
+    await api
+      .post("/api/users")
+      .send(newUser)
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+
+    const usersAtEnd = await helper.usersInDb();
+    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1);
+
+    const usernames = usersAtEnd.map((u) => u.username);
+    expect(usernames).toContain(newUser.username);
+  });
+
+  test("creation fails with proper statuscode and message if username already taken", async () => {
+    const usersAtStart = await helper.usersInDb();
+
+    const newUser = {
+      username: "root",
+      password: "password",
+    };
+
+    const result = await api
+      .post("/api/users")
+      .send(newUser)
+      .expect(400)
+      .expect("Content-Type", /application\/json/);
+
+    expect(result.body.error).toContain("`username` to be unique");
+
+    const usersAtEnd = await helper.usersInDb();
+    expect(usersAtEnd).toHaveLength(usersAtStart.length);
+  });
+});
+
+afterAll(() => {
+  mongoose.connection.close;
+});
